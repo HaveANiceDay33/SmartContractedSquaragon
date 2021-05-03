cc96b2fd5075770e5426593298d1294614fbdf24
kingbd <kingbd@rose-hulman.edu>
2021-05-03 15:01:16 -0400

    just fixed some syntax

--- contracts/Squaragon.sol
+++ contracts/Squaragon.sol
@@ -18,9 +18,9 @@ contract Squaragon {
 	32-29	| board size
 	36-33	| is player 4 to 1 blocked
 	38-37	| winner
-	39		| is game done
-	100-105	| turn
-	*\
+	39		| is game active
+	55-63	| turn
+	*/
 
     constructor () public {
         owner = msg.sender;
@@ -28,9 +28,9 @@ contract Squaragon {
     }
 
 	function move(uint8 direction) public {
-		require( gameState & (0b11<<37))
-		address internal addr = 0;
-		uint8 internal turn = (gameState >> 100) & 0b11;
+		require(gameState & (1<<39));
+		address addr = 0;
+		uint8 turn = (gameState >> 55) & 3;
 		if(turn == 0) {
 			addr = player1;
 		} else if(turn == 1){
@@ -40,9 +40,9 @@ contract Squaragon {
 		} else if(turn == 3){
 			addr = player4;
 		} 
-		uint8 internal offset = 6*turn;
-		uint8 internal row = (gameState >> (offset+3)) & 0b111;
-		uint8 internal col = (gameState >> (offset)) & 0b111;
+		uint8 offset = 6*turn;
+		uint8 row = (gameState >> (offset+3)) & 7;
+		uint8 col = (gameState >> (offset)) & 7;
 		require(addr = msg.sender);
 		if(direction == 0) { //up
 			require(isOpen(--row,col));
@@ -53,32 +53,31 @@ contract Squaragon {
 		} else if(direction == 3) {//left
 			require(isOpen(--row,col));
 		}
-		uint8 internal position = (row << 3) + col;
-		gameState = gameState & ~(0b111111<<offset) | (position<<offset);
-		uint8 internal boardSize = (gameState >> 29) & 0b111;
-		uint8 internal location = (row*boardSize + col)<<3;
-		board = board & ~(0b1111 << location) | (1<<(turn+location));
+		uint8 position = (row << 3) + col;
+		gameState = gameState & ~(63<<offset) | (position<<offset);
+		uint8 boardSize = (gameState >> 29) & 7;
+		uint8 location = (row*boardSize + col)<<3;
+		board = board & ~(15 << location) | (1<<(turn+location));
 		if(isBlocked(turn++)) {
 			if(isBlocked(turn++)) {
 				if(isBlocked(turn++)) {
-					gameState = turn << 37;
-					gameState = gameState | (1<<39);
+					gameState = (turn & 3) << 37;
 				}
 			}			
 		}
-		gameState = gameState & ~(0b11<<100) | (turn<<100);
+		gameState = gameState & ~(3<<55) | (turn<<55);
 	}
 
 
 
-	function isBlocked(uint8 player) private return bool{
-		if((gameState & (1<< (33+player)) != 0) {
+	function isBlocked(uint8 player) private returns (bool){
+		if((gameState & (1<< (33+player))) != 0) {
 			return true;
 		}
-		player = player & 0b11;
-		uint8 internal row = (gameState >> (6*player+3)) & 0b111;
-		uint8 internal col = (gameState >> (6*player)) & 0b111;
-		bool internal result = ~(isOpen(row, col+1) & isOpen(row, col-1) & isOpen(row+1, col)& isOpen(row-1, col));
+		player = player & 3;
+		uint8 row = (gameState >> (6*player+3)) & 7;
+		uint8 col = (gameState >> (6*player)) & 7;
+		bool result = ~(isOpen(row, col+1) & isOpen(row, col-1) & isOpen(row+1, col)& isOpen(row-1, col));
 		if(result) {
 			gameState = gameState | (1<< (33+player));
 		}
@@ -88,9 +87,9 @@ contract Squaragon {
     function start(uint8 gameSize) public {
 		require(msg.sender == owner);
 		require(gameSize-3<=5);
-		require((gameState>>26 && 0b111) == 4);
+		require((gameState>>26 && 7) == 4);
         board = 0;
-		gameState = (gameSize<<29) + (0b100 << 26) + ((gameSize-1) << 21) + ((gameSize-1) << 18) + ((gameSize-1) << 15) + ((gameSize-1) << 9);
+		gameState = (1<<39)+ (gameSize<<29) + (4 << 26) + ((gameSize-1) << 21) + ((gameSize-1) << 18) + ((gameSize-1) << 15) + ((gameSize-1) << 9);
     }
 
     function reset() public {
@@ -99,7 +98,7 @@ contract Squaragon {
     }
 
     function isOpen(uint8 row, uint8 col) private returns (bool){
-		uint8 internal boardSize = (gameState >> 29) & 0b111;
+		uint8 boardSize = (gameState >> 29) & 7;
 		if(row> boardSize || col > boardSize) {
 			return false;
 		}
@@ -107,7 +106,8 @@ contract Squaragon {
     }
 
     function join() public returns (uint8){
-		uint8 internal numOfPlayers = (gameState >> 26) & 0b111;
+		require(gameState & (7 << 37) == 0);
+		uint8 numOfPlayers = (gameState >> 26) & 7;
 		require(numOfPlayers<4);
 		if(numOfPlayers == 0){
             player1 = msg.sender;
@@ -119,7 +119,7 @@ contract Squaragon {
             player4 = msg.sender;
         }
 		numOfPlayers++;
-		gameState = gameState & ~(0b111<<26) | (numOfPlayers<<26);
+		gameState = gameState & ~(7<<26) | (numOfPlayers<<26);
     }
 
 }
